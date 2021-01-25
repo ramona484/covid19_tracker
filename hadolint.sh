fe19ca992d991553aca481f564811f2e85b1a6a6
Ramona84 <ramona.rettig@outlook.de>
2021-01-25 08:30:52 +0100

    added hadolint

--- hadolint.sh
+++ hadolint.sh
@@ -5,7 +5,10 @@ language: generic
 env:
   # Path to 'hadolint' binary
   HADOLINT: "${HOME}/hadolint"
-
+install:
+  # Download hadolint binary and set it as executable
+  - curl -sL -o ${HADOLINT} "https://github.com/hadolint/hadolint/releases/download/v1.17.6/hadolint-$(uname -s)-$(uname -m)"
+    && chmod 700 ${HADOLINT}
 script:
   # List files which name starts with 'Dockerfile'
   # eg. Dockerfile, Dockerfile.build, etc.
