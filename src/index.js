c0bbe886a180409d19045a435445f6c0dc85af37
ramona484 <ramona.rettig@outlook.de>
2021-03-05 07:33:59 +0100

    container scan

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-WEATHER_API_KEY:GQmLiJ4GmWIEGr9BzdWZDN4bwi3mNXmAj2qvTEJqxyqP53DiWmlq3g==
